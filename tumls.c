ac9363ceccc70c3fc41ac647dfe0cef372a55d86
Chandrahas Rachamadugu <tus23882@temple.edu>
2026-09-15 15:47:18 -0400

    Add permission string conversion

--- tumls.c
+++ tumls.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <sys/stat.h>
+
+/* Turns the permission bits into a string such as rwxr-xr-x. */
+void get_permissions(mode_t mode, char permissions[])
+{
+    permissions[0] = (mode & S_IRUSR) ? 'r' : '-';
+    permissions[1] = (mode & S_IWUSR) ? 'w' : '-';
+    permissions[2] = (mode & S_IXUSR) ? 'x' : '-';
+    permissions[3] = (mode & S_IRGRP) ? 'r' : '-';
+    permissions[4] = (mode & S_IWGRP) ? 'w' : '-';
+    permissions[5] = (mode & S_IXGRP) ? 'x' : '-';
+    permissions[6] = (mode & S_IROTH) ? 'r' : '-';
+    permissions[7] = (mode & S_IWOTH) ? 'w' : '-';
+    permissions[8] = (mode & S_IXOTH) ? 'x' : '-';
+    permissions[9] = '\0';
+}
 
 int main(void)
 {
+    char permissions[10];
+
+    get_permissions(0, permissions);
     return 0;
 }
